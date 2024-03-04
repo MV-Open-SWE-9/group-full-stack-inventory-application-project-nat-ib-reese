0e1b0480093fe8f06869f00e5f9a8a82ef34c155
Ibrahim Mohamed <90659607+ibrahimdev23@users.noreply.github.com>
2024-03-04 12:07:05 -0600

    Revert "creating all tests for tier 1"

--- server/index.test.js
+++ server/index.test.js
@@ -1,34 +1,10 @@
 const request = require("supertest");
 const app = require("./app");
 const seed = require("./seed");
-const { Item } = require("./models/index");
-
-let itemQuantity;
 
 // // clear db before tests
 beforeAll(async () => {
   await seed();
-  const items = await Item.findAll({});
-  itemQuantity = items.length;
 });
 
-describe("/items tests", () => {
-  test("GET /items", async () => {
-    const response = await request(app).get("/items");
-    expect(response.body).toContainEqual(
-      expect.objectContaining({
-        name: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-      })
-    );
-  });
-
-  test("GET /items/:id", async () => {
-    const response = await request(app).get("/items/1");
-    expect(response.body).toEqual(
-      expect.objectContaining({
-        name: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-        price: 109.95,
-      })
-    );
-  });
-});
+describe("/items tests", () => {});
