e007c73f3edfe9b7fd507c106168bc11bb770b76
natali-a-lvarez <03nataliaalvarez07@gmail.com>
2024-03-04 15:08:04 -0500

    creating delete route

--- server/routes/items.js
+++ server/routes/items.js
@@ -12,4 +12,15 @@ router.get("/", async (req, res, next) => {
   }
 });
 
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const item = await Item.findByPk(req.params.id);
+    await item.destroy();
+    const items = await Item.findAll();
+    res.send(items);
+  } catch (e) {
+    next(e);
+  }
+});
+
 module.exports = router;
