cc578c8e303866133df22191afe50aad4aa41343
natali-a-lvarez <03nataliaalvarez07@gmail.com>
2024-03-05 15:44:34 -0500

    fixing some conflicts and adding click item to open details functionality

--- public/react/components/ItemInfo.js
+++ public/react/components/ItemInfo.js
@@ -1,18 +1,14 @@
 import React from "react";
 import { useState } from "react";
 
-export const ItemInfo = ({item}) => {
-
-    return (
-        <>  
-        
-            <img src={item.image} alt={item.name}></img>
-            <h1>{item.name}</h1>
-            <h3>{item.category}</h3>
-            <p>{item.description}</p>
-            <p className="item-price">{`$${item.price}`}</p>
-        
-        </>
-    )
-
-}
+export const ItemInfo = ({ item }) => {
+  return (
+    <>
+      <img src={item.image} alt={item.name}></img>
+      <h1>{item.name}</h1>
+      <h3>{item.category}</h3>
+      <p>{item.description}</p>
+      <p className="item-price">{`$${item.price}`}</p>
+    </>
+  );
+};
--- public/react/components/ItemsList.js
+++ public/react/components/ItemsList.js
@@ -1,13 +1,11 @@
-import {React, useState, useEffect} from "react";
-import {Item} from "./Item"
+import { React, useState, useEffect } from "react";
+import { Item } from "./Item";
 import apiURL from "../api";
 import { ItemInfo } from "./ItemInfo";
-export const ItemsList = ({ items }) => {
 
-
-  const [viewItem, setViewItem] = useState("")
-  const [item, setItem]= useState([])
-  const [editItem, setEditItem] = useState([])
+export const ItemsList = ({ items, viewItem, setViewItem }) => {
+  const [item, setItem] = useState([]);
+  const [editItem, setEditItem] = useState([]);
   function deleteItemHandler() {
     console.log("delete");
   }
@@ -20,9 +18,8 @@ export const ItemsList = ({ items }) => {
     try {
       const response = await fetch(`${apiURL}/items/${viewItem}`);
       const item1 = await response.json();
-      setItem(item1)
-      console.log(item1)
-      
+      setItem(item1);
+      console.log(item1);
     } catch (err) {
       console.log("Oh no an error! ", err);
     }
@@ -30,18 +27,24 @@ export const ItemsList = ({ items }) => {
   useEffect(() => {
     fetchItem(viewItem);
   }, [viewItem]);
-  console.log(viewItem)
-  
+  console.log(viewItem);
 
   return (
     <>
-
-      {!viewItem ? items.map((item) => (
-        <Item key={item.id} item={item} onClick={editItemHandler} setViewItem={setViewItem} setEditItem={setEditItem}/ >
-      )): 
-        
-        <ItemInfo key={item.id} item={item}/>
-      }
+      {!viewItem ? (
+        items.map((item) => (
+          <Item
+            key={item.id}
+            item={item}
+            onClick={editItemHandler}
+            setViewItem={setViewItem}
+            setEditItem={setEditItem}
+            fetchItem={fetchItem}
+          />
+        ))
+      ) : (
+        <ItemInfo key={item.id} fetchItem={fetchItem} item={item} />
+      )}
     </>
   );
 };
